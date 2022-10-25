36410bbf1fdced096306a45a8b31c995a516e40d
Trey <phlasc@gmail.com>
2022-10-24 21:55:03 -0400

    Init project

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { ApolloServer } from '@apollo/server';
+import { PrismaClient } from '@prisma/client';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import dotenv from 'dotenv'
+
+const prisma = new PrismaClient();
+
+dotenv.config();
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  type User {
+    id: Int
+    email:String
+    password: String
+    name: String
+  }
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+    users: [User!]!
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chpinzkd',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+    users: () => prisma.user.findMany()
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, { listen: { port: parseInt(process?.env?.NODE_CONTAINER_PORT) ?? 4000 } });
+
+console.log(`🚀 Server reloading hot at: ${url}`);
